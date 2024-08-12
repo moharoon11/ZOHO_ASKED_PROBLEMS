2f937ffec6a3fa4b2b6e9fe779ad5d4638ce2225
moharoon11 <moharoon11107@gmail.com>
2024-08-12 15:50:42 +0530

    batch1

--- src/batch1/KadanesAlgorithm.java
+++ src/batch1/KadanesAlgorithm.java
@@ -1,6 +1,44 @@
 package batch1;
 
+
+// MAXIMUM SUBARRAY SUM
 public class KadanesAlgorithm {
+
+
+    // brute approach
+    // TIME COMPLEXITY: O(n^2)
+    // SPACE COMPLEXITY: O(1)
+    long maxSubarraySum(int[] arr) {
+        long max = Long.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            int sum = 0;
+            for(int j=i; j<arr.length; j++) {
+                sum += arr[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+
+    // OPTIMAL APPROACH
+    // TIME COMPLEXITY: O(N)
+    // SPACE COMPLEXITY: O(1)
+    long maximumSubarraySum(int[] arr) {
+        long maxSum = 0;
+        long maxi = Long.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++) {
+            maxSum += arr[i];
+
+            maxi = Math.max(maxi, maxSum);
+
+            if(maxSum < 0) {
+                maxSum = 0;
+            }
+        }
+
+        return maxi;
+    }
 
     public static void main(String... args) {
 
