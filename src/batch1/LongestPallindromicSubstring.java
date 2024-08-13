80a326101a10e324c84be1e734576df93e7420b3
moharoon11 <moharoon11107@gmail.com>
2024-08-13 15:54:17 +0530

    batch1

---
+++ src/batch1/LongestPallindromicSubstring.java
@@ -0,0 +1,45 @@
+package batch1;
+
+public class LongestPallindromicSubstring {
+
+    /**
+     * brute approach
+     * TIME COMPLEXITY: O(n^2)
+     * SPACE COMPLEXITY: O(1)
+     */
+    static String longestPalin(String s){
+
+        int max = Integer.MIN_VALUE;
+        String lpal = "";
+        for(int i=0; i<s.length(); i++) {
+            for(int j=i; j<s.length(); j++) {
+                String word = s.substring(i, j+1);
+                if(isPalindrome(word)) {
+                    if(word.length() > max) {
+                        max = word.length();
+                        lpal = word;
+                    }
+                }
+            }
+        }
+
+
+        return lpal;
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while(left <= right) {
+            if(word.charAt(left++) != word.charAt(right--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /*************** OPTIMAL APPROACH USING SLIDING WINDOW TECHNIQUE *****************/
+}
